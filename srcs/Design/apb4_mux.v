8bc99b700240351e8e0e20c8be7d7899219df0b8
VigneshCShet <vigneshshet1@gmail.com>
2026-06-02 12:38:45 +0530

    Parameterize apb4_mux for address and data width

--- srcs/Design/apb4_mux.v
+++ srcs/Design/apb4_mux.v
@@ -1,17 +1,22 @@
 `default_nettype none
-module apb4_mux(PADDR, MST_PSEL, SLV_PREADY1, SLV_PRDATA1, SLV_PREADY2, SLV_PRDATA2, MST_PREADY, MST_PRDATA, SLV_PSEL1, SLV_PSEL2, MST_PSLVERR, SLV_PSLVERR1, SLV_PSLVERR2);
+module apb4_mux #(parameter addr_width = 9, data_width = 8)(PADDR, MST_PSEL, SLV_PREADY1, SLV_PRDATA1, SLV_PREADY2, SLV_PRDATA2, MST_PREADY, MST_PRDATA, SLV_PSEL1, SLV_PSEL2, MST_PSLVERR, SLV_PSLVERR1, SLV_PSLVERR2);
+  // Inputs
   input wire  MST_PSEL, SLV_PREADY1, SLV_PREADY2;
-  input wire [8:0] PADDR;
-  input wire [7:0] SLV_PRDATA1, SLV_PRDATA2;
+  input wire [addr_width-1:0] PADDR;
+  input wire [data_width-1:0] SLV_PRDATA1, SLV_PRDATA2;
   input wire SLV_PSLVERR1, SLV_PSLVERR2; 
+  // Outputs
   output reg MST_PREADY, SLV_PSEL1, SLV_PSEL2;
-  output reg [7:0] MST_PRDATA;
+  output reg [data_width-1:0] MST_PRDATA;
   output wire MST_PSLVERR;
 
-  assign MST_PSLVERR = PADDR[8] ? SLV_PSLVERR2 : SLV_PSLVERR1;
+  // Address decoding and multiplexing logic
+  assign MST_PSLVERR = PADDR[addr_width-1] ? SLV_PSLVERR2 : SLV_PSLVERR1;
+
+  // Multiplexing logic for slave selection and data routing
   always @(*) begin
     if(MST_PSEL) begin
-			if(PADDR[8]) begin
+			if(PADDR[addr_width-1]) begin
 				SLV_PSEL1 = 0;
 				SLV_PSEL2 = 1;
 				MST_PREADY = SLV_PREADY2;
